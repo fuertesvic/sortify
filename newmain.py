aed345de07a4c3e09ce56675d58c8a18204e962b
fuertesvic <fuertesvic@gmail.com>
2025-03-10 10:53:52 +0100

    add title label

--- newmain.py
+++ newmain.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QLabel,
+                            QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
 from PyQt6.QtGui import QFont, QIcon
+from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow): # Main window inherits from MainWindow from Qt
     def __init__(self):
@@ -8,9 +10,10 @@ class MainWindow(QMainWindow): # Main window inherits from MainWindow from Qt
         self.setWindowTitle("SortiFy but using Qt!")
         self.setGeometry(0, 0, 800, 600)                   # First 2 are to set the window placing on screen
         self.setWindowIcon(QIcon("icon_for_window"))       # Set Window Icon
-        label = QLabel(self, text="Benvingut/da! Benvingut/da! Benvingut/da! Benvingut/da!")
+        label = QLabel(self, text="Benvingut/da a SortiFy!")
         label.setFont(QFont("Helvetica",25))
-        label.setGeometry(0,0,400,100)
+        label.setGeometry(0,0,800,100)
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
 # Create app - Needed to set widgets on top
 app = QApplication([])
